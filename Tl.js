775c338a032ffb70c92202a552ac381fc5bf2549
Tobinpadavil <139835059+Tobinpadavil@users.noreply.github.com>
2023-07-18 08:53:00 +0530

    Add files via upload

---
+++ Tl.js
@@ -0,0 +1,6 @@
+function openLinks() {
+    var urls = ['https://in.tradingview.com/chart/Lzz0aDdW/', 'https://web.sensibull.com/option-chain?expiry=2023-07-20&tradingsymbol=BANKNIFTY', 'https://www.angelone.in/trade/orders/positions'];
+    for (var i = 0; i < urls.length; i++) {
+      window.open(urls[i], '_blank');
+    }
+  }
