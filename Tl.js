45c6c27ad77bada49e70e2ad7558aced1e95ad9d
Tobinpadavil <139835059+Tobinpadavil@users.noreply.github.com>
2023-09-05 22:25:30 +0530

    Update Tl.js

--- Tl.js
+++ Tl.js
@@ -1,5 +1,5 @@
 function openLinks() {
-    var urls = ['https://in.tradingview.com/chart/Lzz0aDdW/', 'https://web.sensibull.com/option-chain?expiry=2023-07-20&tradingsymbol=BANKNIFTY', 'https://www.angelone.in/trade/orders/positions'];
+    var urls = ['https://in.tradingview.com/chart/QGXJbkmy/', 'https://web.sensibull.com/option-chain?expiry=2023-07-20&tradingsymbol=BANKNIFTY', 'https://www.angelone.in/trade/orders/positions'];
     for (var i = 0; i < urls.length; i++) {
       window.open(urls[i], '_blank');
     }
